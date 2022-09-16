92640f26eff2bcce968bad0edcd706f24303c2aa
Plootle <darian.payan@colorado.edu>
2022-09-16 16:18:35 -0600

    Finished layout. We'll need to rework UML and work together on how to approach a lot of the code.

--- MoveSet.java
+++ MoveSet.java
@@ -3,19 +3,25 @@ public class MoveSet {
      * Based on our UML this will bet where we traverse the map, take fights, find treasure,  and decide outcomes
      * 
      */
-    public void fight( Creatures c, Adventures a)
+
+    public void diceRoll()
     {
 
+    }
+    public void fight( Creatures c, Adventures a)
+    {
+        // get dice roll for creature and adventurer and compare
+        // maybe here we check if sneaker to avoid the fight?!?!?
     }
 
     public void traverse(Adventures a)
     {
-
+        // this might get reworked. essentially this will either be what does ALL movement or have overload calls for creatures since they all move differently
     }
 
     public void treasure(Adventures a)
     {
-        
+        // Cake
     }
 
 }
